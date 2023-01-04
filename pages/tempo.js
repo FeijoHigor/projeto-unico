57ff59057569097a84b85156e659729259d14478
feijohigor <feijohigor@gmail.com>
2023-01-04 18:33:37 -0300

    adiciona revalidate

--- pages/tempo.js
+++ pages/tempo.js
@@ -9,7 +9,7 @@ function Tempo(props) {
                 {dynamicDateString} (dinâmico)
             </div>
             <div>
-                {props.staticDateString} (estático - com delay)
+                {props.staticDateString} (estático)
             </div>
         </div>
     )
@@ -17,19 +17,15 @@ function Tempo(props) {
 
 export async function getStaticProps() {
     console.log('> Passando pelo getStaticProps()')
-    console.log('> Adicionado delay de 5 segundos')
-
-    await delay(5000)
     const staticDate = new Date()
     const staticDateString = staticDate.toString()
 
     return {
         props: {
             staticDateString
-        }
+        },
+        revalidate: 1
     }
 }
-
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 export default Tempo
